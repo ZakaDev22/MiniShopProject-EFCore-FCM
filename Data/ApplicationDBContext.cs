fab69172df23a45d9c9334ecee5d50632a456414
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-02-11 21:59:56 +0000

    Add App.Config.Json File Add The Connection String To json File Add DBContext Class With The UseSql Method To Cennect To The Data base

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace MiniShopProject_EFCore_FCM.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("AppConfig.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(config.GetSection("ConnectionString").Value);
+        }
+    }
+}
