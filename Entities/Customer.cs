697866d3cfa03f07b8e0ad13085f880b4fbb3518
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-02-11 22:03:28 +0000

    Add The Customer And Order Classes

---
+++ Entities/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace MiniShopProject_EFCore_FCM.Entities
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Entities/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniShopProject_EFCore_FCM.Entities
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public decimal Amount { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
